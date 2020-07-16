8562fd39030def79f72d6035d147928c91886c6e
David Felix <david.felix02@hotmail.com>
2020-07-16 20:14:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ end
 
 def monopoly_with_third_tier
   a_hash = monopoly_with_second_tier
-  a_hash[:railroads][:rent_in_dollars] = {:} 
+  a_hash[:railroads][:rent_in_dollars] = {one_piece_owned: 25, one_piece_owned: 25,} 
   
 
 end
